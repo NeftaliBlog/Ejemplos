06c2c410303c74956527fb0db477549a604254ca
German.Estevez <german_ral@hotmail.com>
2019-11-04 20:55:04 +0100

    fichero rsvars.bat

--- Proyecto Pruebas CI/tests/compilar.bat
+++ Proyecto Pruebas CI/tests/compilar.bat
@@ -1,2 +1,2 @@
-call "c:\Program Files\Embarcadero\Studio\20.0\bin\rsvars.bat" 
+call "c:\Program Files (x86)\Embarcadero\Studio\20.0\bin\rsvars.bat" 
 msbuild.exe /nologo /v:m /t:build /p:Config=Release "PTestFunciones.dproj"
