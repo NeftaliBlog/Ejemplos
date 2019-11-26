3225c7ba05165ef0a46b245335b5f2c0d730362f
German.Estevez <german_ral@hotmail.com>
2019-11-26 20:19:40 +0100

    BAT para compilar  el proyecto de pruebvas

--- Proyecto Pruebas CI/compilar.bat
+++ Proyecto Pruebas CI/compilar.bat
@@ -1,13 +1,19 @@
 @echo off
+echo "COMPILANDO PROYECTO "
+echo "--------------------------------------"
+
 REM Si el parámetro es vacío...
 if not x==%1x goto PROGRAMFILESx86
 REM Si el parámetro es x86
 goto PROGRAMFILES
 
 :PROGRAMFILESx86
+echo "Llamando a rsvars"
 call "c:\Program Files (x86)\Embarcadero\Studio\20.0\bin\rsvars.bat" 
 goto BUILD
+
 :PROGRAMFILES
+echo "Llamando a rsvars"
 call "c:\Program Files\Embarcadero\Studio\20.0\bin\rsvars.bat" 
 
 :BUILD
