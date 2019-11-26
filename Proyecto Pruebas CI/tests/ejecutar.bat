598ecac3eb473bf825bb38f992877f61d7e6104a
German.Estevez <german_ral@hotmail.com>
2019-11-26 20:48:58 +0100

    BAT para ejecutar test unitarois

--- Proyecto Pruebas CI/tests/ejecutar.bat
+++ Proyecto Pruebas CI/tests/ejecutar.bat
@@ -1,15 +1,18 @@
 @echo off
+echo "EJECUTANDO TESTS "
+echo "--------------------------------------"
+
 REM Si el parámetro es vacío...  => RELEASE
 if not x==%1x goto RELEASE
 REM Si el parámetro es DEBUG
 goto DEBUG
 
 :RELEASE
-call ".\Win32\Release\PTestFunciones.exe"
+call "c:\PROYECTOS\Proyecto Pruebas CI\tests\Win32\Release\PTestFunciones.exe"
 goto SALIR
 
 :DEBUG
-call ".\Win32\Debug\PTestFunciones.exe"
+call "c:\PROYECTOS\Proyecto Pruebas CI\tests\Win32\Debug\PTestFunciones.exe"
 
 REM finalizar
 :SALIR
