1b3b132d033bbe6079336f5063b95c504022fe64
manu52272@gmail.com <git12345678bup>
2020-05-02 19:47:32 +0200

    Corregir path del proyecto

--- Proyecto Pruebas CI/compilar.bat
+++ Proyecto Pruebas CI/compilar.bat
@@ -17,4 +17,4 @@ echo "Llamando a rsvars"
 call "c:\Program Files\Embarcadero\Studio\20.0\bin\rsvars.bat" 
 
 :BUILD
-msbuild.exe /nologo /v:m /t:build /p:Config=Release "PtestCI.dproj"
+msbuild.exe /nologo /v:m /t:build /p:Config=Release "c:\Proyectos\Proyecto Pruebas CI\PtestCI.dproj" 
